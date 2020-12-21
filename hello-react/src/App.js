ba86b95a6cb37451643a61cc2c79ccef955f6c8e
dongwon <kkum9408@gmail.com>
2020-12-21 23:05:16 +0900

    counter.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import './App.css';
+import Counter from './Counter';
 
-function App() {
-  const name = '리액트';
-
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </div>
-  );
-}
+const App = () => {
+  return <Counter />;
+};
 
 export default App;
