ae959e8788c1d02e8b317f8c1c9fa30624fb9b2a
dongwon <kkum9408@gmail.com>
2020-12-22 21:01:55 +0900

    chater 3. component, props, state

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -13,7 +13,10 @@ class Counter extends Component {
         <h2>바뀌지 않는 값 : {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 }, () => {
+              console.log('방금 setState 호출');
+              console.log(this.state);
+            });
           }}
         >
           +1
