9c32991e90b24a5031c8cd8ed196f56d30a95586
dongwon <kkum9408@gmail.com>
2020-12-24 02:44:33 +0900

    chapter 7. component lifecycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import IteractionSample from './IteractionSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+    console.log('111', this.color);
+  };
+
   render() {
-    return <IteractionSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
   }
 }
 
