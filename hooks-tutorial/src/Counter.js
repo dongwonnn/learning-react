8189cf4542dd19e0d5ee6c970553e65cfb6974ba
dongwon <kkum9408@gmail.com>
2020-12-27 23:05:08 +0900

    hoos-tutorial

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -8,7 +8,7 @@ const Counter = () => {
         현재 카운터 값은 <b>{value}</b>입니다.
       </p>
       <button onClick={() => setValue(value + 2)}>+2</button>
-      <button onClick={() => setValue(value - 1)}>-1</button>
+      <button onClick={() => setValue(value - 2)}>-2</button>
     </div>
   );
 };
