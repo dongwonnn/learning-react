f4c2ab567bef7541c754bf91d55ac78a586fb95d
dongwon <kkum9408@gmail.com>
2021-01-03 04:35:21 +0900

    chatpter 9.4

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import CSSModule from './CSSModule';
+import StyleComponent from './StyleComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule />
+        <StyleComponent />
       </div>
     );
   }
