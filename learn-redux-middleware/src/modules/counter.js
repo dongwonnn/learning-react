dc00e7442ba2f4927a0cf0d678be57f7a75de14c
dongwon <kkum9408@gmail.com>
2021-01-20 00:24:51 +0900

    chpater 18 fin

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,22 +1,51 @@
 import { createAction, handleActions } from 'redux-actions';
+import { delay, put, takeEvery, takeLatest, select } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
+
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+function* increaseSaga() {
+  yield delay(1000); // 1초 기다림
+  yield put(increase()); // 특정 액션을 디스패치
+  const number = yield select((state) => state.counter);
+  console.log(`현재 값은 ${number}입니다.`);
+}
+
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* conuterSaga() {
+  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리
+  // 두번 클릭하면 두번 다 실행
+  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+
+  // takeLatest는 기존에 진행 중이던 작업이 있다면 취소하고 가장 마지막으로 실행된 작업만 수행
+  // 두번 클릭해도 한번만 실행
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
+
+// export const increaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(increase());
+//   }, 1000);
+// };
+
+// export const decreaseAsync = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(decrease());
+//   }, 1000);
+// };
 
 // export const increase = () => ({type : INCREASE})
 // export const decrease = () => ({type : DECREASE})
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,7 +1,11 @@
-import { handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
 // api 객체 형태로 가져오기
+//import createRequestThunk from '../lib/createRequestThunk';
+
+import createRequestSaga from '../lib/createRequestSaga';
+import { call, put, takeLatest } from 'redux-saga/effects';
+// import { startLoading, finishLoading } from './loading';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -13,9 +17,59 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수 생성
 // 시작, 성공, 실패했을 때 다른 액션을 디스패치
+// export const getPost = createRequestThunk(GET_POST, api.getPost);
+// export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
+
+// function* getPostSaga(action) {
+//   yield put(startLoading(GET_POST));
+
+//   try {
+//     const post = yield call(api.getPost, action.payload);
+//     yield put({
+//       type: GET_POST_SUCCESS,
+//       payload: post.data,
+//     });
+//   } catch (e) {
+//     yield put({
+//       type: GET_POST_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//   }
+//   yield put(finishLoading(GET_POST));
+// }
+
+// function* getUsersSaga(action) {
+//   yield put(startLoading(GET_USERS));
+
+//   try {
+//     const users = yield call(api.getUsers);
+//     yield put({
+//       type: GET_USERS_SUCCESS,
+//       payload: users.data,
+//     });
+//   } catch (e) {
+//     yield put({
+//       type: GET_USERS_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//   }
+//   yield put(finishLoading(GET_USERS));
+// }
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
+
+// ---------------------------------------------------------------------
 
 // export const getPost = (id) => async (dispatch) => {
 //   dispatch({ type: GET_POST }); // 시작
